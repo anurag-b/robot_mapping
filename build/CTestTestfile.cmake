28c80879c5dc0dfac4d2abb33a795647a62eeeed
anurag-b <anuragbansal4@gmail.com>
2018-03-04 23:49:11 -0500

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/anuragb/ros_prac/catkin_ws_p13/src
+# Build directory: /home/anuragb/ros_prac/catkin_ws_p13/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(project_4)
